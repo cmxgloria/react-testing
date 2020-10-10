de4bacad1fb4c56e0fd49d77e88443f2ce9b1774
Gloria Chen <cmxgloria@gmail.com>
2020-10-10 19:19:27 +1100

    fix: Add div

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Button label="Click Me Please" />
-      </header>
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <Button label="Click Me Please" />
+        </header>
+      </div>
     </div>
   );
 }
